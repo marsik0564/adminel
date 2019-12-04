8f6af5c5b333d74576de8935fe3b8de44be4f2ac
marsik <marsik_0564@mail.ru>
2019-12-04 21:58:32 +0200

    day 6

--- app/Http/Controllers/Blog/Admin/MainController.php
+++ app/Http/Controllers/Blog/Admin/MainController.php
@@ -4,13 +4,39 @@ namespace App\Http\Controllers\Blog\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Repositories\Admin\MainRepository;
+use App\Repositories\Admin\OrderRepository;
+use App\Repositories\Admin\ProductRepository;
 use MetaTag;
 
 class MainController extends AdminBaseController
 {
+    private $orderRepository;
+    private $productRepository;
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $this->orderRepository = app(OrderRepository::class);
+        $this->productRepository = app(ProductRepository::class);
+        
+    }
+    
     public function index()
     {
+        $countOrders = MainRepository::getCountOrders();
+        $countUsers = MainRepository::getCountUsers();
+        $countProducts = MainRepository::getCountProducts();
+        $countCategories = MainRepository::getCountCategories();
+        
+        $perpage = 4;
+        
+        $last_orders = $this->orderRepository->getAllOrders($perpage+1);
+        $last_products = $this->productRepository->getLastProducts($perpage);
+        
         MetaTag::setTags(['title' => 'Админ панель']);
-        return view('blog.admin.main.index');
+        return view('blog.admin.main.index', 
+            compact('countOrders', 'countUsers', 'countProducts', 
+                'countCategories', 'last_orders', 'last_products'));
     }
 }
